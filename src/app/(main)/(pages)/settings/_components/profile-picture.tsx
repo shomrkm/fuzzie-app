de8c95f729a19aaf8f643d551b925fbc2b2d8d25
shomrkm <shochan1985@gmail.com>
2024-09-07 16:45:00 +0900

    wip: updating Setting page.

--- src/app/(main)/(pages)/settings/_components/profile-picture.tsx
+++ src/app/(main)/(pages)/settings/_components/profile-picture.tsx
@@ -1,15 +1,52 @@
 'use client'
 import React from "react";
 import UploadcareButton from "./uploadcare-button";
+import { useRouter } from "next/navigation";
+import Image from "next/image";
+import { Button } from "@/components/ui/button";
+import { FileUploaderRegular } from "@uploadcare/react-uploader";
 
-type Props = {};
+type Props = {
+  userImage: string
+  onUpload?: any
+  onDelete?: any
+};
 
-export const ProfilePicture = (props: Props) => {
+export const ProfilePicture = ({ userImage, onUpload, onDelete }: Props) => {
+  const router = useRouter()
+
+  const onRemoveProfileImage = () => {
+    const response = onDelete()
+    if(response){
+      router.refresh()
+    }
+  }
+
   return (
     <div className="flex flex-col">
       <p className="text-lg text-white">Profile Picture</p>
       <div className="flex h-[30vh] flex-col items-center justify-center">
-        <UploadcareButton />
+        {
+          userImage ? (
+            <>
+              <div className="relative h-full w-2/12">
+                <Image
+                  src={userImage}
+                  alt="User_Image"
+                  fill
+                />
+                <Button
+                  onClick={onRemoveProfileImage}
+                  className="bg-transparent text-white/70 hover:bg-transparent hover:text-white"
+                >
+                  Remove Logo
+                </Button>
+              </div>
+            </>
+          ) : (
+            <UploadcareButton />
+          )
+        }
       </div>
     </div>
   )
--- src/app/(main)/(pages)/settings/page.tsx
+++ src/app/(main)/(pages)/settings/page.tsx
@@ -5,7 +5,8 @@ import { ProfilePicture } from "./_components/profile-picture";
 type Props = {};
 
 const Settings = (props: Props) => {
-  // WIP wire picture
+
+
   return <div className="flex flex-col gap-4">
     <h1 className="sticky top-0 z-[10] flex items-center justify-between border-b bg-background/50 p-6 text-4xl backdrop-blur-lg">
       <span>Settings</span>
@@ -15,7 +16,9 @@ const Settings = (props: Props) => {
         <h1 className="text-2xl font-bold">User Profile</h1>
         <p className="text-base text-white/50">Add or update your information</p>
       </div>
-      <ProfilePicture />
+      <ProfilePicture
+        userImage={user?.profilePicture}
+      />
       <ProfileForm />
     </div>
   </div>
